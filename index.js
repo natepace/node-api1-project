a8497b427e5bf6ee65ad9dbd5b39ef4633bf97bb
natepace <n8wpace@gmail.com>
2021-04-05 20:09:29 -0600

    finished mvp

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const server = require('./api/server');
+const server = require('./api/server.js');
 
 const port = 5000;
 
-// START YOUR SERVER HERE
+// // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log("your server runs on 5000, my lord")
+})
+// // server.listen(999, () => {
+//     console.log("running on port 999")
+// })
+// console.log('hi')
